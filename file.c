61c04ac00c0effb82d2bb1524286b038064679b7
SShayashi <SShayashi0208@gmail.com>
2015-07-11 18:43:16 +0900

    とりあえず

--- file.c
+++ file.c
@@ -46,6 +46,7 @@ static Buffer *bufferListHead = NULL;
  */
 static Buffer *bufferListTail = NULL;
 
+/*git テスト*/
 /*
  * initializeBufferList -- バッファリストの初期化
  *
@@ -115,7 +116,7 @@ static Result initializeBufferList()
  *	buf: リストの先頭に移動させるバッファへのポインタ
  *
  * 返り値:
- *	なし
+ *	なしです
  */
 static void moveBufferToListHead(Buffer *buf)
 {
