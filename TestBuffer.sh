32a740f747c1b63b5bef92730fea50938c810f98
SShayashi <SShayashi0208@gmail.com>
2016-01-02 22:25:52 +0900

    無駄ファイルの削除

--- TestBuffer.sh
+++
@@ -1,4 +0,0 @@
-cc -o file.o -g -c file.c
-cc -o test-buffer.o -g -c test-buffer.c
-cc -o FileBuffer -g  file.o test-buffer.o
-./FileBuffer
--- TestManip.sh
+++
@@ -1,4 +0,0 @@
-cc -o data-manip.o -g -c datamanip.c
-cc -o test-datamanip.o -g -c test-datamanip.c
-cc -o TestDatamanip -g  datamanip.o test-datamanip.o
-./TestDatamanip
